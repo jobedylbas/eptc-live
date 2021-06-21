6ec8a7dbfaf1b0059770802bd42f4a70e11d73c6
Jobe Diego Dylbas dos Santos <jobe.dylbas@inf.ufrgs.br>
2021-06-20 23:49:10 -0300

    bug: correct type of incidentMetric Model

--- src/models/incidentMetrics.js
+++ src/models/incidentMetrics.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const IncidentMetricsSchema = new Schema({
     date: { type: Date, required: true, default: Date() },
-    type: { type: String, required: true, default: '26a0' },
+    type: { type: Number, required: true, default: 1 },
     hasAddress: { type: Boolean, require: true, default: false },
     isLocalized: { type: Boolean, require: true, default: false},
     id: { type: String, require: true }
